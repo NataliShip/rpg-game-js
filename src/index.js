20eabea245f428d7c14cdd0cc505b43395573c4a
Nataliya Shipelova <natulechka_m.25@mail.ru>
2021-07-08 18:10:42 +0300

    add background

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import './index.scss'
 import femaleWalk from './assets/Female-2-Walk.png'
+import terrainAtlas from './assets/terrain.png'
+import worldCfg from './configs/world.json'
+import sprites from './configs/sprites'
 
 const canvas = document.getElementById('game')
 const ctx = canvas.getContext('2d')
@@ -12,6 +15,19 @@ let cycle = 0
 let moveDirection = null
 let pY = canvasHeight / 2 - spriteHeight / 2
 let pX = canvasWidth / 2 - spriteWidth / 2
+
+const terrain = document.createElement('img')
+terrain.src = terrainAtlas
+
+terrain.addEventListener('load', () => {
+  const {map} = worldCfg
+  map.forEach((cfgRow, y) => {
+    cfgRow.forEach((cfgCell, x) => {
+      const [sX, sY, sW, sH] = sprites.terrain[cfgCell[0]].frames[0]
+      ctx.drawImage(terrain, sX, sY, sW, sH, x * spriteWidth, y * spriteHeight, spriteWidth, spriteHeight)
+    })
+  })
+})
 
 const DIRECTIONS = {
   bottom: 0,
@@ -78,7 +94,7 @@ img.addEventListener('load', () => {
         break
       default:
     }
-    ctx.clearRect(0, 0, canvasWidth, canvasHeight)
+    // ctx.clearRect(0, 0, canvasWidth, canvasHeight)
     const dir = DIRECTIONS[moveDirection] || 0
     ctx.drawImage(img, cycle * spriteWidth, dir, spriteWidth, spriteHeight, pX, pY, 48, 48)
   }, 120)
